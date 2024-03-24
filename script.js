dd857cf3c1efd3111f117d3837c42afdb546a2b9
Stephzennn <estifanosSNzen@outlook.com>
2024-03-24 04:00:10 +0300

    create 16x16 grids

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 
 
+const Container = document.querySelector("#Container");
+
+
+
+for (let k = 0; k < 16 ; k++ ){
+    for (let k = 0; k < 16 ; k++ ){
+        let div = document.createElement('div')
+        div.classList = "cell"
+        
+        Container.appendChild(div)
+    }
+}
